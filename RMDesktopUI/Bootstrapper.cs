da3956b5ae4d8416a27f5bea0b97d11e30ed7031
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-07-12 13:31:46 +0300

    Configure container

--- RMDesktopUI/Bootstrapper.cs
+++ RMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using Caliburn.Micro;
 using RMDesktopUI.ViewModels;
 
@@ -6,14 +8,36 @@ namespace RMDesktopUI
 {
 	public class Bootstrapper : BootstrapperBase
 	{
+		private SimpleContainer _container = new SimpleContainer();
+
 		public Bootstrapper()
 		{
 			Initialize();
 		}
 
+		protected override void Configure()
+		{
+			_container.Instance(_container);
+		}
+
 		protected override void OnStartup(object sender, StartupEventArgs e)
 		{
 			DisplayRootViewFor<ShellViewModel>();
+		}
+
+		protected override object GetInstance(Type service, string key)
+		{
+			return _container.GetInstance(service, key);
+		}
+
+		protected override IEnumerable<object> GetAllInstances(Type service)
+		{
+			return _container.GetAllInstances(service);
+		}
+
+		protected override void BuildUp(object instance)
+		{
+			_container.BuildUp(instance);
 		}
 	}
 }
