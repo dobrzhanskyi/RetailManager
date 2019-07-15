adeb66a674be726027ad11a2ca798bbb9df63d4b
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-07-15 16:24:50 +0300

    SQL Tables added

---
+++ RMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductID] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [PurchaseDate] DATETIMEOFFSET NOT NULL DEFAULT getutcdate()
+)
