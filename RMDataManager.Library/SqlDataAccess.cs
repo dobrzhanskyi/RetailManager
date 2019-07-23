b1aa8a04b6cfe8ac62353e7888e5a8b876c0e5b7
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-07-23 15:51:14 +0300

    Adding library and getting connection string.Adding Dapper from nuget

---
+++ RMDataManager.Library/SqlDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace RMDataManager.Library
+{
+	public class SqlDataAccess
+	{
+		public string GetConnectionString(string name)
+		{
+			return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+		}
+	}
+}
