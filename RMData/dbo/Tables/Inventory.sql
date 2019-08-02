1f2c1d90de4bf37b66575e18d92f2fcd8af39289
Vladimir Dobrzhanskyi <33500265+dobrzhanskyi@users.noreply.github.com>
2019-08-02 14:11:07 +0300

    Update Inventory.sql

--- RMData/dbo/Tables/Inventory.sql
+++ RMData/dbo/Tables/Inventory.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Inventory]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ProductID] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1, 
     [PurchasePrice] MONEY NOT NULL, 
