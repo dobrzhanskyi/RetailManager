8a7f02489068dcdd088a49740c822f650573d28f
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-08-23 16:21:21 +0300

    Add Transactions
    Add transactions to sql DataAcces and used them for salesData Call

--- RMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ RMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
@@ -7,8 +8,11 @@ using Dapper;
 
 namespace RMDataManager.Library.Internal.DataAccess
 {
-	internal class SqlDataAccess
+	internal class SqlDataAccess : IDisposable
 	{
+		private IDbConnection _connection;
+		private IDbTransaction _transaction;
+
 		public string GetConnectionString(string name)
 		{
 			return ConfigurationManager.ConnectionStrings[name].ConnectionString;
@@ -34,6 +38,45 @@ namespace RMDataManager.Library.Internal.DataAccess
 				connection.Execute(storedProcedure, parameters,
 				   commandType: CommandType.StoredProcedure);
 			}
+		}
+
+		public void StartTransaction(string connectionStringName)
+		{
+			var connectionString = GetConnectionString(connectionStringName);
+			_connection = new SqlConnection(connectionString);
+			_connection.Open();
+			_transaction = _connection.BeginTransaction();
+		}
+
+		public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+		{
+			_connection.Execute(storedProcedure, parameters,
+			   commandType: CommandType.StoredProcedure, transaction: _transaction);
+		}
+
+		public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+		{
+			List<T> rows = _connection.Query<T>(storedProcedure, parameters,
+				commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+
+			return rows;
+		}
+
+		public void CommitTransaction()
+		{
+			_transaction?.Commit();
+			_connection?.Close();
+		}
+
+		public void RollbackTransaction()
+		{
+			_transaction?.Rollback();
+			_connection?.Close();
+		}
+
+		public void Dispose()
+		{
+			CommitTransaction();
 		}
 	}
 }
