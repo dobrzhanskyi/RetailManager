e04294d0946984dbbaf24e5c233ed736bfa016cc
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-09-03 17:40:37 +0300

    Add Admin EndPointData
    Adds the data for administrative endpont.
    Does not yet secure the endpoint Authorisation

--- RMDataManager/Controllers/SaleController.cs
+++ RMDataManager/Controllers/SaleController.cs
@@ -1,13 +1,21 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 using Microsoft.AspNet.Identity;
 using RMDataManager.Library.DataAccess;
 using RMDataManager.Library.Models;
 
 namespace RMDataManager.Controllers
 {
-	[Authorize]
+	//[Authorize]
 	public class SaleController : ApiController
 	{
+		[Route("GetSalesReport")]
+		public List<SaleReportModel> GetSalesReport()
+		{
+			SaleData data = new SaleData();
+			return data.GetSaleReport();
+		}
+
 		public void Post(SaleModel sale)
 		{
 			SaleData data = new SaleData();
